04e2c039db779daad0ad46815400acd10034fdc8
idn-fahmifn <fahmifn.idn@gmail.com>
2025-09-17 15:19:15 +0700

    3.1

---
+++ database/migrations/2025_09_17_080249_create_ruangan_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ruangan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user')->constrained('users')->cascadeOnDelete(); //untuk pic ruangan
+            $table->string('nama_ruangan');
+            $table->enum('ukuran', ['kecil', 'sedang', 'besar'])->default('kecil');
+            $table->text('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ruangan');
+    }
+};
---
+++ database/migrations/2025_09_17_080257_create_barang_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_ruangan')->constrained('ruangan')->cascadeOnDelete(); //untuk pic ruangan
+            $table->string('nama_barang');
+            $table->string('merk');
+            $table->string('gambar');
+            $table->date('tanggal_pembelian');
+            $table->enum('kondisi', ['baik', 'rusak', 'sedang diperbaiki'])->default('baik');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang');
+    }
+};
