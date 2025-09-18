f1a117895cf6ce104c90405e7b44f6b8b5cf237d
idn-fahmifn <fahmifn.idn@gmail.com>
2025-09-18 14:40:36 +0700

    4.7

--- resources/views/admin/ruangan/detail.blade.php
+++ resources/views/admin/ruangan/detail.blade.php
@@ -33,6 +33,52 @@
                                 <p class="text-md text-gray-900 dark:text-gray-400 mt-2">
                                     Deskripsi : <span class="font-bold">{{ $data->deskripsi }}</span>
                                 </p>
+
+                                <form action="#" method="post" class="py-6">
+                                    <button type="submit"
+                                        class="rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20">Hapus</button>
+                                    <button x-data=""
+                                        x-on:click.prevent="$dispatch('open-modal', 'show-edit')"
+                                        class="rounded-md bg-red-500 px-2.5 py-1.5 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20">Edit</button>
+
+                                    {{-- modal --}}
+                                    <x-modal name="show-edit" :show="$errors->userDeletion->isNotEmpty()" focusable>
+                                        <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
+                                            @csrf
+                                            @method('delete')
+
+                                            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                                {{ __('Are you sure you want to delete your account?') }}
+                                            </h2>
+
+                                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                                            </p>
+
+                                            <div class="mt-6">
+                                                <x-input-label for="password" value="{{ __('Password') }}"
+                                                    class="sr-only" />
+
+                                                <x-text-input id="password" name="password" type="password"
+                                                    class="mt-1 block w-3/4" placeholder="{{ __('Password') }}" />
+
+                                                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+                                            </div>
+
+                                            <div class="mt-6 flex justify-end">
+                                                <x-secondary-button x-on:click="$dispatch('close')">
+                                                    {{ __('Cancel') }}
+                                                </x-secondary-button>
+
+                                                <x-danger-button class="ms-3">
+                                                    {{ __('Delete Account') }}
+                                                </x-danger-button>
+                                            </div>
+                                        </form>
+                                    </x-modal>
+
+                                </form>
+
                             </div>
                         </div>
                     </div>
